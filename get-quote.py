ef80bc9205b1942829265ecc1caffabef9c717f5
FFFF-0000h <adebiyiitunuayo@gmail.com>
2022-08-06 07:33:55 +0100

    UpdateRandom quote bot

--- get-quote.py
+++ get-quote.py
@@ -2,13 +2,21 @@ import random
 def primary():
  # print("Keep it logically awesome.")
 
+  f = open("quotes.txt", "a")
+  text= "To live is Christ, to die is gain\n"
+  f.write(text)
+  f.close()
+
+
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  last = 13
+
+  last = len(quotes) - 1
   rnd = random.randint(0, last)
-  print(quotes[rnd])
+  rnd1 = random.randint(0, last)
+  print(quotes[rnd].rstrip("\n"),quotes[rnd1].rstrip("\n"))
 
 if __name__== "__main__":
   primary()
